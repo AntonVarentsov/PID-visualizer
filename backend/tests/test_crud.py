2e1e81f3a4f32f9d55409c551c696021a884f035
varentsov <varentsov@gmail.com>
2025-06-24 10:57:58 +0300

    Обновлены пути импорта в тестах для корректной работы с модулем backend. Обновлен бинарный файл кэша.

--- backend/tests/test_crud.py
+++ backend/tests/test_crud.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'backend')))
 
 import pytest
 from backend import crud, schemas
